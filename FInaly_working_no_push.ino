246241c12f80c698aacb493311eec6dd57d1b693
Akuma <aeres99@hotmail.com>
2019-03-12 10:50:00 +0530

    Add files via upload

---
+++ FInaly_working_no_push.ino
@@ -0,0 +1,77 @@
+#include <SoftwareSerial.h>
+#define RX 10
+#define TX 11
+String AP = "Aeres";       // CHANGE ME
+String PASS = "FuCky0u@$$#0le"; // CHANGE ME
+String API = "8CI4162Y3W43KGEE";   // CHANGE ME
+String HOST = "api.thingspeak.com";
+String PORT = "80";
+String field = "field1";
+int countTrueCommand;
+int countTimeCommand; 
+boolean found = false; 
+int valSensor = 1;
+SoftwareSerial esp8266(RX,TX); 
+const int trigPin = 9;
+const int echoPin = 8;
+long duration;
+int distance;
+void setup() {
+pinMode(trigPin, OUTPUT); 
+pinMode(echoPin, INPUT); // Sets the echoPin as an Input
+Serial.begin(9600); // Starts the serial communication
+esp8266.begin(115200);
+  sendCommand("AT",5,"OK");
+  sendCommand("AT+CWMODE=1",5,"OK");
+  sendCommand("AT+CWJAP=\""+ AP +"\",\""+ PASS +"\"",20,"OK");
+}
+void loop() {
+digitalWrite(trigPin, LOW); // Clears the trigPin
+delayMicroseconds(5); 
+digitalWrite(trigPin, HIGH);  
+delayMicroseconds(40);
+digitalWrite(trigPin, LOW); 
+duration = pulseIn(echoPin, HIGH);  
+distance= (duration*0.034/2);
+delay(1000);
+Serial.print("Distance: ");
+Serial.println(distance);
+ valSensor = distance;
+ String getData = "GET /update?api_key="+ API +"&"+ field +"="+String(valSensor);
+sendCommand("AT+CIPMUX=1",5,"OK");
+ sendCommand("AT+CIPSTART=0,\"TCP\",\""+ HOST +"\","+ PORT,15,"OK");
+ sendCommand("AT+CIPSEND=0," +String(getData.length()+4),4,">");
+ esp8266.println(getData);
+ delay(1500);
+ countTrueCommand++;
+ sendCommand("AT+CIPCLOSE=0",5,"OK");
+}
+void sendCommand(String command, int maxTime, char readReplay[]) {
+  Serial.print(countTrueCommand);
+  Serial.print(". at command => ");
+  Serial.print(command);
+  Serial.print(" ");
+  while(countTimeCommand < (maxTime*1))
+  {
+    esp8266.println(command);                //at+cipsend
+    if(esp8266.find(readReplay))              //ok
+    {
+      found = true;
+      break;
+    }
+    countTimeCommand++;
+  }
+  if(found == true)
+  {
+    Serial.println("OYI");
+    countTrueCommand++;
+    countTimeCommand = 0;
+  }
+  if(found == false)
+  {
+    Serial.println("Fail");
+    countTrueCommand = 0;
+    countTimeCommand = 0;
+  }
+  found = false;
+ }
